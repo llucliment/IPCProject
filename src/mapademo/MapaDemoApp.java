2ab9f0a44a1713d09d9e9245de652c8957ef4968
laurapatriciaantoneac-ctrl <laurapatriciaantoneac@gmail.com>
2026-05-19 19:02:20 +0200

    Added the dashboard link so when you registre it chanlleges the window to the dashboard

--- src/mapademo/MapaDemoApp.java
+++ src/mapademo/MapaDemoApp.java
@@ -1,5 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
+ *
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
@@ -14,10 +14,6 @@ import javafx.stage.Stage;
 
 import java.util.HashMap;
 
-/**
- *
- * @author jose
- */
 public class MapaDemoApp extends Application {
     public static Scene scene;
 
@@ -82,5 +78,3 @@ public class MapaDemoApp extends Application {
     }
 
 }
-
-
